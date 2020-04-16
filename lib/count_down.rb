e6fc13c259389a254957a55f99877ddf620ab828
Dan Love <daniel.love.jr@gmail.com>
2020-04-16 19:27:27 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,13 @@
 # Write your code here
+<<<<<<< HEAD
 count = 10 
 while count >= 1 do
   puts count
   count -= 1
 end
 puts "Happy New Year!"
+=======
+while -1 do
+  puts "blah blah blah"
+end
+>>>>>>> 9acd9f17f0357584a61e8ec22f6f991c74ab4b15
